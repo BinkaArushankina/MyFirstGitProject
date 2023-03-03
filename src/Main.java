b941128c487f809e427a279444e47a8b4349e14a
BinkaArushankina <albinarushankina@gmail.com>
2023-03-03 10:55:49 +0100

    first commit for my git project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+
+
+
+        /*
+        *.class budet ignorirowatj wse faili class
+        .idea/ budet ignirirowatj
+        polnij spisok ignorow nuschno w glawnij direktorij prisobatschitj
+
+        ili
+        * w github kogda delaesch nowij repositorij ignore node sdelatj
+
+         */
     }
 }
